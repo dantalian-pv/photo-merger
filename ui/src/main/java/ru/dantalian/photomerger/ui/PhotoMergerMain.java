7e394157e8475c7e3d301915b0e2f65f90a5b5bc
dantalain-pv <dantalian.pv@gmail.com>
2018-09-21 10:25:13 +0200

    Draft for using swt
    Signed-off-by: dantalain-pv <dantalian.pv@gmail.com>

--- ui/src/main/java/ru/dantalian/photomerger/ui/PhotoMergerMain.java
+++ ui/src/main/java/ru/dantalian/photomerger/ui/PhotoMergerMain.java
@@ -1,9 +1,18 @@
 package ru.dantalian.photomerger.ui;
 
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
 public class PhotoMergerMain {
 
 	public static void main(String[] args) {
-		new MainFrame().init();
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) display.sleep();
+		}
+		display.dispose();
 	}
 
 }
